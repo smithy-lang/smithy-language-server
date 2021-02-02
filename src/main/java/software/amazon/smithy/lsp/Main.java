94dc2f26d86d5695608a8d281c900e43fa446a8f
Chase Coalwell <chasecoa@amazon.com>
2021-02-02 14:25:23 -0800

    Use System.in and System.out by configuration

--- src/main/java/software/amazon/smithy/lsp/Main.java
+++ src/main/java/software/amazon/smithy/lsp/Main.java
@@ -15,6 +15,10 @@
 
 package software.amazon.smithy.lsp;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.launch.LSPLauncher;
 import org.eclipse.lsp4j.services.LanguageClient;
@@ -29,18 +33,46 @@ public class Main {
    * @param args Arguments passed to launch server.
    */
   public static void main(String[] args) {
+
+    Socket socket = null;
+    InputStream in;
+    OutputStream out;
+
     try {
+      String port = args[0];
+      // If port is set to "0", use System.in/System.out.
+      if (port.equals("0")) {
+        in = System.in;
+        out = System.out;
+      } else {
+        socket = new Socket("localhost", Integer.parseInt(port));
+        in = socket.getInputStream();
+        out = socket.getOutputStream();
+      }
       SmithyLanguageServer server = new SmithyLanguageServer();
-      Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, System.in, System.out);
+      Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, in, out);
       LanguageClient client = launcher.getRemoteProxy();
 
       server.connect(client);
 
       launcher.startListening().get();
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Missing port argument");
+    } catch (NumberFormatException e) {
+      System.out.println("Port number must be a valid integer");
     } catch (Exception e) {
       System.out.println(e);
 
       e.printStackTrace();
+    } finally {
+      try {
+        if (socket != null) {
+          socket.close();
+        }
+      } catch (Exception e) {
+        System.out.println("Failed to close the socket");
+        System.out.println(e);
+      }
     }
   }
 }
