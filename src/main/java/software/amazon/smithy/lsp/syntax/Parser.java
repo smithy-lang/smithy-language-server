33bf8eb2cd6a9b025b7035e0f05f480e23549d8d
Joe Wu <yizhouwu@amazon.com>
2025-02-06 11:06:31 -0800

    Removed comments for private methods, switch case default actions. Modified import orders. Avoided create Range object in folding range creation. Added comment to previous Parser class changes of enum.

--- src/main/java/software/amazon/smithy/lsp/syntax/Parser.java
+++ src/main/java/software/amazon/smithy/lsp/syntax/Parser.java
@@ -815,7 +815,7 @@ final class Parser extends SimpleParser {
             Syntax.Ident name = ident();
             var enumMemberDef = new Syntax.Statement.EnumMemberDef(parent, name);
             enumMemberDef.start = start;
-            setEnd(enumMemberDef);
+            setEnd(enumMemberDef); // Set the enumMember end right after ident processed for simple enum member.
             addStatement(enumMemberDef);
 
             ws();
@@ -823,7 +823,7 @@ final class Parser extends SimpleParser {
                 skip(); // '='
                 ws();
                 enumMemberDef.value = parseNode();
-                setEnd(enumMemberDef);
+                setEnd(enumMemberDef); // Override the previous enumMember end if assignment exists.
             }
         } else {
             addErr(position(), position(),
