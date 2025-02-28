2b30740ca08b839ebaf7423bc3dc55c8f41328d3
Miles Ziemer <mziemer@amazon.com>
2025-02-28 15:03:09 -0500

    Improve document/documentSymbol
    When I re-wrote the language feature implementations in #166, I
    refactored documentSymbol. But it mostly just copied the previous
    implementation, which produced a flat list of symbols. This meant that
    member symbols would appear at the top-level, but documentSymbol is
    meant to provide a tree-like view, where symbols can have children. So
    this commit re-writes documentSymbol to produce a list of only the
    top-level shapes' symbols (and the namespace statement's symbol), with
    members' symbols being added as children to the top-level symbols, and
    members of inline i/o are children of the inline i/o symbol.
    
    I also changed up the Symbol kind a bit, so root shapes are of type
    `Class` (except enum/intEnum, which are of type `Enum`), members are
    of type `Field` (except enum/intEnum members, which are of type
    `EnumMember`), and service/resource/operation members are of type
    `Property`.
    
    I explored having different symbol kinds for different types of shapes,
    like making service-type shapes be of kind `Interface`, and primitive
    types being of their corresponding kind (for example, an integer shape
    would have kind `Number`), but I wasn't sure what some shape types
    should be. For example, what should a `blob` shape be? So I decided to
    just make all top-level shapes just be of kind `Class`.
    
    I also partially fixed the range of symbols, so the range covers the
    whole statement, and selectionRange just the identifier (i.e. shape
    name). I'm not sure if the range of top-level symbols should include the
    range of their children though, but I probably should include the range
    of traits and docs.
    
    I'm considering adding more children for service-type shape members,
    like making the `operations` child symbol of a service shape have a
    child for each of the operations in the list, but I'm not sure if that
    is desireable.

--- src/main/java/software/amazon/smithy/lsp/language/DocumentSymbolHandler.java
+++ src/main/java/software/amazon/smithy/lsp/language/DocumentSymbolHandler.java
@@ -5,59 +5,187 @@
 
 package software.amazon.smithy.lsp.language;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.function.Consumer;
 import org.eclipse.lsp4j.DocumentSymbol;
-import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.SymbolInformation;
 import org.eclipse.lsp4j.SymbolKind;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import software.amazon.smithy.lsp.document.Document;
-import software.amazon.smithy.lsp.protocol.LspAdapter;
 import software.amazon.smithy.lsp.syntax.Syntax;
 
 public record DocumentSymbolHandler(Document document, List<Syntax.Statement> statements) {
+    private static final EnumSet<Syntax.Statement.Type> MEMBER_TYPES = EnumSet.of(
+            Syntax.Statement.Type.MemberDef,
+            Syntax.Statement.Type.EnumMemberDef,
+            Syntax.Statement.Type.ElidedMemberDef,
+            Syntax.Statement.Type.InlineMemberDef,
+            Syntax.Statement.Type.NodeMemberDef
+    );
+    private static final EnumSet<Syntax.Statement.Type> INLINE_TYPES = EnumSet.of(
+            Syntax.Statement.Type.ForResource,
+            Syntax.Statement.Type.Mixins,
+            Syntax.Statement.Type.Block,
+            Syntax.Statement.Type.TraitApplication,
+            Syntax.Statement.Type.MemberDef,
+            Syntax.Statement.Type.ElidedMemberDef
+    );
+
     /**
      * @return A list of DocumentSymbol
      */
     public List<Either<SymbolInformation, DocumentSymbol>> handle() {
-        return statements.stream()
-                .mapMulti(this::addSymbols)
-                .toList();
+        List<Either<SymbolInformation, DocumentSymbol>> result = new ArrayList<>();
+        addSymbols((symbol) -> result.add(Either.forRight(symbol)));
+        return result;
     }
 
-    private void addSymbols(Syntax.Statement statement, Consumer<Either<SymbolInformation, DocumentSymbol>> consumer) {
-        switch (statement) {
-            case Syntax.Statement.TraitApplication app -> addSymbol(consumer, app.id(), SymbolKind.Class);
+    private void addSymbols(Consumer<DocumentSymbol> consumer) {
+        DocumentSymbol parent = null;
 
-            case Syntax.Statement.ShapeDef def -> addSymbol(consumer, def.shapeName(), SymbolKind.Class);
-
-            case Syntax.Statement.EnumMemberDef def -> addSymbol(consumer, def.name(), SymbolKind.Enum);
-
-            case Syntax.Statement.ElidedMemberDef def -> addSymbol(consumer, def.name(), SymbolKind.Property);
-
-            case Syntax.Statement.MemberDef def -> {
-                addSymbol(consumer, def.name(), SymbolKind.Property);
-                if (def.target() != null) {
-                    addSymbol(consumer, def.target(), SymbolKind.Class);
+        var listIterator = statements.listIterator();
+        while (listIterator.hasNext()) {
+            var statement = listIterator.next();
+            if (statement instanceof Syntax.Statement.Namespace namespace) {
+                consumer.accept(namespaceSymbol(namespace));
+            } else if (statement instanceof Syntax.Statement.ShapeDef shapeDef) {
+                parent = rootSymbol(shapeDef);
+                consumer.accept(parent);
+            } else if (parent != null && MEMBER_TYPES.contains(statement.type())) {
+                if (statement instanceof Syntax.Statement.InlineMemberDef inlineMemberDef) {
+                    inlineMemberSymbol(listIterator, parent, inlineMemberDef);
+                } else {
+                    addMemberSymbol(parent, statement);
                 }
             }
-            default -> {
-            }
         }
     }
 
-    private void addSymbol(
-            Consumer<Either<SymbolInformation, DocumentSymbol>> consumer,
-            Syntax.Ident ident,
-            SymbolKind symbolKind
-    ) {
-        Range range = LspAdapter.identRange(ident, document);
-        if (range == null) {
-            return;
+    private DocumentSymbol namespaceSymbol(Syntax.Statement.Namespace namespace) {
+        return new DocumentSymbol(
+                namespace.namespace().stringValue(),
+                SymbolKind.Namespace,
+                document.rangeBetween(namespace.start(), namespace.end()),
+                document.rangeBetween(namespace.namespace().start(), namespace.namespace().end())
+        );
+    }
+
+    private DocumentSymbol rootSymbol(Syntax.Statement.ShapeDef shapeDef) {
+        return new DocumentSymbol(
+                shapeDef.shapeName().stringValue(),
+                getSymbolKind(shapeDef),
+                document.rangeBetween(shapeDef.start(), shapeDef.end()),
+                document.rangeBetween(shapeDef.shapeName().start(), shapeDef.shapeName().end())
+        );
+    }
+
+    private static SymbolKind getSymbolKind(Syntax.Statement.ShapeDef shapeDef) {
+        return switch (shapeDef.shapeType().stringValue()) {
+            case "enum", "intEnum" -> SymbolKind.Enum;
+            default -> SymbolKind.Class;
+        };
+    }
+
+    private void addMemberSymbol(DocumentSymbol parent, Syntax.Statement statement) {
+        if (parent.getChildren() == null) {
+            parent.setChildren(new ArrayList<>());
         }
 
-        DocumentSymbol symbol = new DocumentSymbol(ident.stringValue(), symbolKind, range, range);
-        consumer.accept(Either.forRight(symbol));
+        DocumentSymbol memberSymbol = switch (statement) {
+            case Syntax.Statement.MemberDef memberDef -> memberDefSymbol(memberDef);
+
+            case Syntax.Statement.EnumMemberDef enumMemberDef -> enumMemberDefSymbol(enumMemberDef);
+
+            case Syntax.Statement.ElidedMemberDef elidedMemberDef -> elidedMemberDefSymbol(elidedMemberDef);
+
+            case Syntax.Statement.NodeMemberDef nodeMemberDef -> nodeMemberDefSymbol(nodeMemberDef);
+
+            default -> null;
+        };
+
+        if (memberSymbol != null) {
+            parent.getChildren().add(memberSymbol);
+        }
+    }
+
+    private DocumentSymbol memberDefSymbol(Syntax.Statement.MemberDef memberDef) {
+        return new DocumentSymbol(
+                memberDef.name().stringValue(),
+                SymbolKind.Field,
+                document.rangeBetween(memberDef.start(), memberDef.end()),
+                document.rangeBetween(memberDef.name().start(), memberDef.name().end()),
+                memberDef.target() == null ? null : memberDef.target().stringValue()
+        );
+    }
+
+    private DocumentSymbol enumMemberDefSymbol(Syntax.Statement.EnumMemberDef enumMemberDef) {
+        return new DocumentSymbol(
+                enumMemberDef.name().stringValue(),
+                SymbolKind.EnumMember,
+                document.rangeBetween(enumMemberDef.start(), enumMemberDef.end()),
+                document.rangeBetween(enumMemberDef.name().start(), enumMemberDef.name().end())
+        );
+    }
+
+    private DocumentSymbol elidedMemberDefSymbol(Syntax.Statement.ElidedMemberDef elidedMemberDef) {
+        var range = document.rangeBetween(elidedMemberDef.start(), elidedMemberDef.end());
+        return new DocumentSymbol(
+                elidedMemberDef.name().stringValue(),
+                SymbolKind.Field,
+                range,
+                range
+        );
+    }
+
+    private DocumentSymbol nodeMemberDefSymbol(Syntax.Statement.NodeMemberDef nodeMemberDef) {
+        var range = document.rangeBetween(nodeMemberDef.start(), nodeMemberDef.end());
+        return new DocumentSymbol(
+                nodeMemberDef.name().stringValue(),
+                SymbolKind.Property,
+                range,
+                range
+        );
+    }
+
+    private void inlineMemberSymbol(
+            ListIterator<Syntax.Statement> listIterator,
+            DocumentSymbol parent,
+            Syntax.Statement.InlineMemberDef inlineMemberDef
+    ) {
+        if (parent.getChildren() == null) {
+            parent.setChildren(new ArrayList<>());
+        }
+
+        var inlineSymbol = new DocumentSymbol(
+                inlineMemberDef.name().stringValue(),
+                SymbolKind.Property,
+                document.rangeBetween(inlineMemberDef.start(), inlineMemberDef.end()),
+                document.rangeBetween(inlineMemberDef.name().start(), inlineMemberDef.name().end())
+        );
+        parent.getChildren().add(inlineSymbol);
+
+        List<DocumentSymbol> children = new ArrayList<>();
+
+        while (listIterator.hasNext()) {
+            var statement = listIterator.next();
+
+            if (!INLINE_TYPES.contains(statement.type())) {
+                listIterator.previous();
+                break;
+            }
+
+            if (statement instanceof Syntax.Statement.MemberDef memberDef) {
+                children.add(memberDefSymbol(memberDef));
+            } else if (statement instanceof Syntax.Statement.ElidedMemberDef elidedMemberDef) {
+                children.add(elidedMemberDefSymbol(elidedMemberDef));
+            }
+        }
+
+        if (!children.isEmpty()) {
+            inlineSymbol.setChildren(children);
+        }
     }
 }
